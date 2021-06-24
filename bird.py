3540a4016b3f0b8bc73b1cb1fbd6b5359f1eb33d
kumarharsh2396 <kumarharsh2396@gmail.com>
2021-06-24 18:15:49 +0530

    commit

--- bird.py
+++ bird.py
@@ -59,6 +59,15 @@ def frames_to_msec(frames, fps=FPS):
     fps: The framerate to use for conversion.  Default: FPS.
     """
     return 1000.0 * frames / fps
+
+def msec_to_frames(milliseconds, fps=FPS):
+    """Convert milliseconds to frames at the specified framerate.
+
+    Arguments:
+    milliseconds: How many milliseconds to convert to frames.
+    fps: The framerate to use for conversion.  Default: FPS.
+    """
+    return fps * milliseconds / 1000.0
             
 
 def main():
