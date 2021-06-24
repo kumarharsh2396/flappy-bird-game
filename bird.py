62cd546fd2d86618f816539353c4235693ce4309
kumarharsh2396 <kumarharsh2396@gmail.com>
2021-06-24 18:18:23 +0530

    initial commit

--- bird.py
+++ bird.py
@@ -11,6 +11,59 @@ ANIMATION_SPEED = 0.18 #pixels per milisec
 WIN_WIDTH = 284 * 2 #Background image sie: 284 * 512; tilled twice
 WIN_HEIGHT = 512
 
+class Bird(pygame.sprite.Sprite):
+    """Represents the bird controlled by the player.
+
+    The bird is the 'hero' of this game.  The player can make it climb
+    (ascend quickly), otherwise it sinks (descends more slowly).  It must
+    pass through the space in between pipes (for every pipe passed, one
+    point is scored); if it crashes into a pipe, the game ends.
+
+    Attributes:
+    x: The bird's X coordinate.
+    y: The bird's Y coordinate.
+    msec_to_climb: The number of milliseconds left to climb, where a
+        complete climb lasts Bird.CLIMB_DURATION milliseconds.
+
+    Constants:
+    WIDTH: The width, in pixels, of the bird's image.
+    HEIGHT: The height, in pixels, of the bird's image.
+    SINK_SPEED: With which speed, in pixels per millisecond, the bird
+        descends in one second while not climbing.
+    CLIMB_SPEED: With which speed, in pixels per millisecond, the bird
+        ascends in one second while climbing, on average.  See also the
+        Bird.update docstring.
+    CLIMB_DURATION: The number of milliseconds it takes the bird to
+        execute a complete climb.
+    """
+
+    WIDTH = HEIGHT = 50
+    SINK_SPEED = 0.18
+    CLIMB_SPEED = 0.3
+    CLIMB_DURATION = 333.3
+
+    def __init__(self, x, y, msec_to_climb, images):
+        """Initialise a new Bird instance.
+
+        Arguments:
+        x: The bird's initial X coordinate.
+        y: The bird's initial Y coordinate.
+        msec_to_climb: The number of milliseconds left to climb, where a
+            complete climb lasts Bird.CLIMB_DURATION milliseconds.  Use
+            this if you want the bird to make a (small?) climb at the
+            very beginning of the game.
+        images: A tuple containing the images used by this bird.  It
+            must contain the following images, in the following order:
+                0. image of the bird with its wing pointing upward
+                1. image of the bird with its wing pointing downward
+        """
+        super(Bird, self).__init__()
+        self.x, self.y = x, y
+        self.msec_to_climb = msec_to_climb
+        self._img_wingup, self._img_wingdown = images
+        self._mask_wingup = pygame.mask.from_surface(self._img_wingup)
+        self._mask_wingdown = pygame.mask.from_surface(self._img_wingdown)
+
 def load_images():
     """Load all images required by the game and return a dict of them.
 
